b1b51c06ae91d78a70e67afec6e34e4c13cddaca
louis-heer <louis.heer381@myci.csuci.edu>
2026-06-10 14:27:25 -0600

    Started working on checking and credit accounts

--- BankAccount.hpp
+++ BankAccount.hpp
@@ -61,9 +61,67 @@ class SavingsAccount : public BankAccount {
 };
 
 class CheckingAccount : public BankAccount {
+public:
+    using BankAccount::BankAccount;
 
+    void deposit(double amount) override {
+        balance += amount;
+    }
+
+    void withdraw(double amount) override {
+        if (amount > balance) {
+            std::cout << "Insufficient funds!" << std::endl;
+            return;
+            balance -= amount;
+        }
+    }
+    void cashOutCheck(double amount) override {
+        if (amount > balance) {
+            std::cout << "Insufficient funds!" << std::endl;
+            return;
+            balance -= amount;
+        }
+    }
+
+    void printStatement() const override {
+        std::cout << "Checking Account Statement" << std::endl;
+        std::cout << "Account Number: " << accountNumber << std::endl;
+        std::cout << "Account Holder: " << accountHolderName << std::endl;
+        std::cout << "Balance: $" << balance << std::endl;
+        std::cout << "Your earnings this month: $" << balance * interestRate << std::endl;
+    }
 };
 
 class CreditAccount : public BankAccount {
+private:
+    double interestRate;
+    double availableCredit;
+public:
+    using BankAccount::BankAccount;
 
+    void deposit(double amount) override {
+        balance += amount;
+    }
+
+    void withdraw(double amount) override {
+        if (amount > balance) {
+            std::cout << "Insufficient funds!" << std::endl;
+            return;
+            balance -= amount;
+        }
+    }
+    void calculateInterest() {
+        balance += balance * interestRate;
+    }
+
+    void calculateAvailablCredit() {
+
+    }
+
+    void printStatement() const override {
+        std::cout << "Credit Account Statement" << std::endl;
+        std::cout << "Account Number: " << accountNumber << std::endl;
+        std::cout << "Account Holder: " << accountHolderName << std::endl;
+        std::cout << "Balance: $" << balance << std::endl;
+    }
 };
